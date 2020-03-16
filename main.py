e816cc4772fe745492609988a7f606d3d6d52e74
Tomás Ahumada <tomyahu@gmail.com>
2020-03-16 16:52:31 -0300

    added small script that runs a tas with the wrapper

---
+++ main.py
@@ -0,0 +1,16 @@
+from lib.os_lib.cd import cd
+from subprocess import Popen, PIPE, TimeoutExpired
+
+love_path = "C:\Program Files\love_versions\love-0.10.0-win64\love.exe"
+
+with cd("./love_ga_wrapper"):
+    p = Popen([love_path, ".", "run_tas"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+    try:
+        out, err = p.communicate(timeout=10)
+    except TimeoutExpired:
+        p.kill()
+        out, err = p.communicate()
+
+    print("out:\n", out.decode())
+    print("err:\n", err.decode())
