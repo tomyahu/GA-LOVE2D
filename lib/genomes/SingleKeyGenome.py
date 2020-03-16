97f931bf5a75d78cdac82f3adff1641a643b1e66
Tomás Ahumada <tomyahu@gmail.com>
2020-03-16 14:18:43 -0300

    documented and refactored single key genome

--- lib/genomes/SingleKeyGenome.py
+++ lib/genomes/SingleKeyGenome.py
@@ -2,35 +2,41 @@ from ga_settings.word_list import word_list
 from random import random, randint
 
 
-# param: input:str -> the input key to press
-# A single input key genome, represents a key press for one frame
 class SingleKeyGenome():
+    """
+    A single input key genome, represents a key press for one frame
+    """
 
-    def __init__(self, input):
-        self.input = input
+    def __init__(self, key_input):
+        """
+        :param key_input: <str> the input key to press
+        """
+        self.key_input = key_input
 
-    # crossOver: SingleKeyGenome -> SingleKeyGenome
-    # Returns a new Int Genome with the mean of the both
     def cross_over(self, genome):
-        new_input = ''
+        """
+        Returns a new Single Key Genome with one of the inputs
+        :param genome: <SingleKeyGenome>
+        :return: <SingleKeyGenome> a new Single Key Genome with one of the inputs
+        """
+        new_input = self.key_input
 
         if random() < 0.5:
-            new_input = self.input
-        else:
-            new_input = genome.input
+            new_input = genome.get_key_input()
 
         return SingleKeyGenome(new_input)
 
-    # mutate: None -> None
-    # Mutates the current genome
     def mutate(self):
-        self.input = word_list[randint(0, len(word_list) - 1)]
-
-    # mutateWithMaxFrame: int -> None
-    # Performs a regular mutation
-    def mutate_with_max_frame(self, max_frame):
-        self.mutate()
+        """
+        Mutates the current genome
+        """
+        self.key_input = word_list[randint(0, len(word_list) - 1)]
 
     # getter
+    @staticmethod
     def get_frames(self):
         return 1
+
+    # getter
+    def get_key_input(self):
+        return self.key_input
