f423d2f5c461937b5610195367196b859df7d908
Tomás Ahumada <tomyahu@gmail.com>
2020-03-17 17:52:29 -0300

    added tostring and save_to_file methods for input scripts

--- lib/input_scripts/InputScript.py
+++ lib/input_scripts/InputScript.py
@@ -1,14 +1,29 @@
-# TODO: Document this
+
 class InputScript:
+    """
+    Class for managing input scripts
+    """
 
     def __init__(self):
         self.input_dict = {}
 
     def add_input(self, input_key, start_frame, end_frame):
+        """
+        Adds an input to the input script
+        :param input_key: <str> the key that corresponds to the input
+        :param start_frame: <int> the start frame of the input
+        :param end_frame: <int> the end frame of the input
+        """
         self.add_input_aux(input_key, start_frame, True)
         self.add_input_aux(input_key, end_frame, False)
 
     def add_input_aux(self, input_key, frame, val):
+        """
+        Auxiliar method for add_input
+        :param input_key: <str> the key that corresponds to the input
+        :param frame: <int> the start frame of the input
+        :param val: <bool> the value to save on the frame for that input (true for press and false for release)
+        """
         if not (frame in self.input_dict):
             self.input_dict[frame] = {}
 
@@ -26,6 +41,23 @@ class InputScript:
                 new_input_script.add_input_aux(input_key, frame, val)
 
         return new_input_script
+
+    def __str__(self):
+        input_dict_string = self.input_dict.__str__()
+        input_dict_string.replace("True", "true")
+        input_dict_string.replace("False", "false")
+        return input_dict_string
+
+    def save_to_file(self, path):
+        """
+        Saves the input script to a path
+        :param path: <str> the path of the file on which to save the input string
+        """
+        input_dict_string = self.__str__()
+        file = open(path, "w")
+        file.write(input_dict_string)
+        file.close()
+
 
     def get_input_dict(self):
         return self.input_dict
