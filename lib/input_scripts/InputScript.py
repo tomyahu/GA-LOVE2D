ab99f882c82222b2cbe1259582a2d80417d79cc8
Tomás Ahumada <tomyahu@gmail.com>
2020-03-17 17:10:24 -0300

    added add operation to input scripts

--- lib/input_scripts/InputScript.py
+++ lib/input_scripts/InputScript.py
@@ -5,11 +5,27 @@ class InputScript:
         self.input_dict = {}
 
     def add_input(self, input_key, start_frame, end_frame):
-        self._add_input_aux(input_key, start_frame, True)
-        self._add_input_aux(input_key, end_frame, False)
+        self.add_input_aux(input_key, start_frame, True)
+        self.add_input_aux(input_key, end_frame, False)
 
-    def _add_input_aux(self, input_key, frame, val):
+    def add_input_aux(self, input_key, frame, val):
         if not (frame in self.input_dict):
             self.input_dict[frame] = {}
 
         self.input_dict[frame][input_key] = val
+
+    def __add__(self, other):
+        new_input_script = InputScript()
+
+        for frame, input_dict in self.input_dict:
+            for input_key, val in input_dict:
+                new_input_script.add_input_aux(input_key, frame, val)
+
+        for frame, input_dict in other.get_input_dict():
+            for input_key, val in input_dict:
+                new_input_script.add_input_aux(input_key, frame, val)
+
+        return new_input_script
+
+    def get_input_dict(self):
+        return self.input_dict
