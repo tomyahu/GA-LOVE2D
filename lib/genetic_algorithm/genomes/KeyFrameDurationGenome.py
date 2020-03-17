210ade7b969879078365f27c727292f26b39f905
Tomás Ahumada <tomyahu@gmail.com>
2020-03-17 15:43:23 -0300

    added mutate with max frame method to key frame duration genome

--- lib/genetic_algorithm/genomes/KeyFrameDurationGenome.py
+++ lib/genetic_algorithm/genomes/KeyFrameDurationGenome.py
@@ -1,3 +1,5 @@
+import random
+
 from lib.input_scripts.InputScript import InputScript
 from lib.genetic_algorithm.genomes.KeyFrameGenome import KeyFrameGenome
 
@@ -15,6 +17,20 @@ class KeyFrameDurationGenome(KeyFrameGenome):
         """
         KeyFrameGenome.__init__(self, key_input, frame)
         self.frame_duration = frame_duration
+
+    def mutate_with_max_frame(self, max_frame):
+        """
+        Mutates the frame and duration of the individual and changes it to a random space between 1 and the max frame
+        :param max_frame: <int> the maximum frame that the genome can work on
+        """
+        random_frame_1 = random.randint(1,max_frame)
+        random_frame_2 = random.randint(1,max_frame)
+
+        min_random_frame = min(random_frame_1, random_frame_2)
+        max_random_frame = max(random_frame_1, random_frame_2)
+
+        self.frame = min_random_frame
+        self.frame_duration = max_random_frame - min_random_frame
 
     def get_inputs(self):
         """
