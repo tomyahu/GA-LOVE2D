45c3f4e60b73bad6555942dc44078eecc748e742
Tomás Ahumada <tomyahu@gmail.com>
2020-03-17 12:36:16 -0300

    added input key individual

---
+++ lib/genetic_algorithm/individuals/InputKeyIndividual.py
@@ -0,0 +1,33 @@
+from lib.genetic_algorithm.individuals.InputIndividual import InputIndividual
+
+
+class InputKeyIndividual(InputIndividual):
+    """
+    An individual for the genetic algorithm, it represents an input script. Its composed by only single key genomes
+    """
+
+    def __init__(self, genomes):
+        """
+        :param genomes: <list(SingleKeyGenome)> The list of genomes of the current individual
+        """
+        InputIndividual.__init__(self, genomes)
+
+    def get_inputs(self):
+        """
+        Gets the input script of the individual
+        :return: <InputScript> the input script that this individual represents
+        """
+        inputs = dict()
+        inputs[0] = dict()
+
+        for i in range(self.genomes):
+            genome_inputs = self.genomes[i].get_inputs()
+
+            inputs[i + 1] = dict()
+
+            for key_input, status in genome_inputs:
+                inputs[i][key_input] = status
+                if status:
+                    inputs[i+1][key_input] = False
+
+        return inputs
