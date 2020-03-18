3424725d3c46bf0816bb25bd289f1e13a654e86a
Tomás Ahumada <tomyahu@gmail.com>
2020-03-18 15:51:50 -0300

    added mutation function

---
+++ lib/genetic_algorithm/mutation_function/MutationFunction.py
@@ -0,0 +1,12 @@
+
+class MutationFunction:
+    """
+    A dummy class for mutation functions
+    """
+
+    def mutate(self, individual):
+        """
+        mutates an individual, in this case it does nothing
+        :param individual: <InputIndividual> the individual to perform mutation on
+        """
+        pass
