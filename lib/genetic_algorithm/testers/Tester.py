67e96f324e3553eeca2a6ad1f3ee97a5a3227b80
Tomás Ahumada <tomyahu@gmail.com>
2020-03-18 17:48:14 -0300

    documented tester

--- lib/genetic_algorithm/testers/Tester.py
+++ lib/genetic_algorithm/testers/Tester.py
@@ -4,13 +4,10 @@ class Tester:
     Dummy tester class for testing individuals and get their fitness
     """
 
-    def __init__(self):
-        pass
-
     def test(self, individual):
         """
         Test an individual and returns their fitness
-        :param individual: <InputIndividual>
+        :param individual: <InputIndividual> the individual to be tested
         :return: <num> the fitness of the individual
         """
         pass
