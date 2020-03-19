a9a0b2468cef03fe8c90a4f2894bb91b74906945
Tomás Ahumada <tomyahu@gmail.com>
2020-03-19 15:44:29 -0300

    added population factory

---
+++ lib/genetic_algorithm/population/PopulationFactory.py
@@ -0,0 +1,25 @@
+from lib.genetic_algorithm.crossover_functions.SinglePointCrossoverFunction import SinglePointCrossoverFunction
+from lib.genetic_algorithm.mutation_functions.RegularMutationFunction import RegularMutationFunction
+from lib.genetic_algorithm.population.Population import Population
+from lib.genetic_algorithm.selection_functions.TournamentSelectionFunction import TournamentSelectionFunction
+
+
+class PopulationFactory:
+
+	@staticmethod
+	def get_classic_population(individuals, tester, mutation_prob, elitism_ratio):
+		population = Population(individuals, tester, elitism_ratio)
+
+		# Regular Mutation Function
+		mutation_manager = population.get_mutation_manager()
+		mutation_manager.set_mutation_fun(RegularMutationFunction(mutation_prob))
+
+		# Tournament Selection with tournaments of size 4
+		selection_manager = population.get_selection_manager()
+		selection_manager.set_selection_fun(TournamentSelectionFunction(4))
+
+		# Single Point Crossover Function
+		crossover_manager = population.get_crossover_manager()
+		crossover_manager.set_crossover_fun(SinglePointCrossoverFunction())
+
+		return population
