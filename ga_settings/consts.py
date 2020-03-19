9d4460ec7766e6db24b8eea2ed317578d2561f4a
Tomás Ahumada <tomyahu@gmail.com>
2020-03-19 15:43:19 -0300

    added genetic algorithm constants to consts

--- ga_settings/consts.py
+++ ga_settings/consts.py
@@ -1,1 +1,9 @@
 love_path = "C:\Program Files\love_versions\love-0.10.0-win64\love.exe"
+
+generations = 100
+individual_num = 100
+mutation_prob = 0.05
+elitism_ratio = 0.05
+
+#frames_to_skip = 570
+frames_to_test = 1000
