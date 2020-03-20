65e42839927beca039dfff0de2a757c072a0d4d0
Tomás Ahumada <tomyahu@gmail.com>
2020-03-20 12:51:58 -0300

    added skip process

--- lib/genetic_algorithm/testers/LoveTester.py
+++ lib/genetic_algorithm/testers/LoveTester.py
@@ -1,5 +1,6 @@
-from ga_settings.consts import love_path, frames_to_clean, frames_to_test
+from ga_settings.consts import love_path, frames_to_clean, frames_to_test, frames_to_skip
 from lib.genetic_algorithm.testers.Tester import Tester
+from lib.input_scripts.InputScript import InputScript
 from lib.os_lib.cd import cd
 from subprocess import Popen, PIPE, TimeoutExpired
 
@@ -17,18 +18,21 @@ class LoveTester(Tester):
         """
 
         # Get input script and save it as out
-        input_script = individual.get_inputs()
+        original_input_script = individual.get_inputs()
+        shifted_input_script = original_input_script.shift_frames(frames_to_skip)
+        skip_input_script = InputScript("individuals/skip")
+
+        input_script = shifted_input_script + skip_input_script
         input_script.save_to_file("individuals/out")
 
         with cd("./love_ga_wrapper"):
             p = Popen([love_path, ".", "run_tas", "clean", str(frames_to_clean)], stdin=PIPE, stdout=PIPE, stderr=PIPE)
             p.communicate()
 
-            p = Popen([love_path, ".", "run_tas", "test", str(frames_to_test)], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            p = Popen([love_path, ".", "run_tas", "out", str(frames_to_test + frames_to_skip)], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
             try:
                 out, err = p.communicate()
-                print(float(out), err)
             except TimeoutExpired:
                 p.kill()
                 out, err = p.communicate()
