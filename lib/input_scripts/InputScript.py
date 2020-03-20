fdd1dd4a66e43013f668bb1fac4c804e0331488e
Tomás Ahumada <tomyahu@gmail.com>
2020-03-20 12:49:39 -0300

    added shift frames method and import from file to input script lib

--- lib/input_scripts/InputScript.py
+++ lib/input_scripts/InputScript.py
@@ -1,11 +1,22 @@
+import ast
+
 
 class InputScript:
     """
     Class for managing input scripts
     """
 
-    def __init__(self):
-        self.input_dict = {}
+    def __init__(self, file_path=None):
+        if file_path == None:
+            self.input_dict = dict()
+            return
+
+        f = open(file_path)
+        input_script_str = f.read()
+        input_script_str = input_script_str.replace("false", "False")
+        input_script_str = input_script_str.replace("true", "True")
+
+        self.input_dict = ast.literal_eval(input_script_str)
 
     def add_input(self, input_key, start_frame, end_frame):
         """
@@ -25,27 +36,28 @@ class InputScript:
         :param val: <bool> the value to save on the frame for that input (true for press and false for release)
         """
         if not (frame in self.input_dict):
-            self.input_dict[frame] = {}
+            self.input_dict[frame] = dict()
 
         self.input_dict[frame][input_key] = val
 
     def __add__(self, other):
         new_input_script = InputScript()
 
-        for frame, input_dict in self.input_dict:
-            for input_key, val in input_dict:
+        for frame, input_dict in self.input_dict.items():
+            for input_key, val in input_dict.items():
                 new_input_script.add_input_aux(input_key, frame, val)
 
-        for frame, input_dict in other.get_input_dict():
-            for input_key, val in input_dict:
+        for frame, input_dict in other.get_input_dict().items():
+            for input_key, val in input_dict.items():
                 new_input_script.add_input_aux(input_key, frame, val)
 
         return new_input_script
 
     def __str__(self):
         input_dict_string = self.input_dict.__str__()
-        input_dict_string.replace("True", "true")
-        input_dict_string.replace("False", "false")
+        input_dict_string = input_dict_string.replace("True", "true")
+        input_dict_string = input_dict_string.replace("False", "false")
+        input_dict_string = input_dict_string.replace("'", "\"")
         return input_dict_string
 
     def save_to_file(self, path):
@@ -60,3 +72,12 @@ class InputScript:
 
     def get_input_dict(self):
         return self.input_dict
+
+    def shift_frames(self, frames_shifted):
+        new_input_script = InputScript()
+        for frame, input_dict_list in self.input_dict.items():
+            for input_key, val in input_dict_list.items():
+                new_input_script.add_input_aux(input_key, frame + frames_shifted, val)
+
+        return new_input_script
+
