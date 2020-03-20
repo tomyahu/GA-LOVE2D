b38e113d3b380476b7b26d0c0d286402424ae514
Tomás Ahumada <tomyahu@gmail.com>
2020-03-20 12:47:48 -0300

    added frames to skip

--- ga_settings/consts.py
+++ ga_settings/consts.py
@@ -1,10 +1,10 @@
 love_path = "C:\Program Files\love_versions\love-0.10.0-win64\love.exe"
 
-generations = 100
+generations = 30
 individual_num = 100
 mutation_prob = 0.05
 elitism_ratio = 0.05
 
-#frames_to_skip = 570
 frames_to_clean = 200
+frames_to_skip = 800
 frames_to_test = 1000
