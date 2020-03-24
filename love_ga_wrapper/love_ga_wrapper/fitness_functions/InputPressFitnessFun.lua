16a46da0ada36e4b1397900af2d21f5c90bc38b5
Tomás Ahumada <tomyahu@gmail.com>
2020-03-24 17:47:21 -0300

    renamed input count fitness fun and fixed documentation to it

--- love_ga_wrapper/love_ga_wrapper/fitness_functions/InputPressFitnessFun.lua
+++ love_ga_wrapper/love_ga_wrapper/fitness_functions/InputPressFitnessFun.lua
@@ -3,21 +3,24 @@ local extend = require("love_ga_wrapper.lib.classes.extend")
 local FitnessFun = require("love_ga_wrapper.fitness_functions.FitnessFun")
 -------------------------------------------------------------
 
--- class: InputCountFitnessFun
-local InputCountFitnessFun = extend(FitnessFun, function(self, input_tracked)
+-- class: InputPressFitnessFun
+-- param: input_tracked:str -> the type of input tracked
+-- a fitness function that counts how many times an input was activated in an individual (this just counts the button
+-- presses, it doesn't count the releases and holds)
+local InputPressFitnessFun = extend(FitnessFun, function(self, input_tracked)
     self.input_tracked = input_tracked
     self.input_number = 0
 end)
 
 -- initAux: None -> None
--- The auxiliary initialization function for the fitness calculation, saves the current amount of asteroids
-function InputCountFitnessFun.initAux(self)
+-- The auxiliary initialization function for the fitness calculation, resets the input number
+function InputPressFitnessFun.initAux(self)
     self.input_number = 0
 end
 
 -- mainFun: None -> num
--- The main fitness function, gets the difference between the current and the asteroid number
-function InputCountFitnessFun.mainFun(self)
+-- The main fitness function, gets all the times the assigned input was pressed
+function InputPressFitnessFun.mainFun(self)
     local tas_inputs = self.individual_tas:getInputs()
     
     for frame, inputs_in_frame in pairs(tas_inputs) do
@@ -31,7 +34,7 @@ end
 
 -- stepFun: None -> None
 -- The step fitness function, does nothing
-function InputCountFitnessFun.stepFun(self)
+function InputPressFitnessFun.stepFun(self)
 end
 
-return InputCountFitnessFun
+return InputPressFitnessFun
