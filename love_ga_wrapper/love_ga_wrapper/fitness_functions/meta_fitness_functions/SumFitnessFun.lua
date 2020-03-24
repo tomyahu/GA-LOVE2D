4251d9cdc6338c3fc0200cad54da410811a4faf3
Tomás Ahumada <tomyahu@gmail.com>
2020-03-24 17:51:05 -0300

    updated sum fitness fun documentation

--- love_ga_wrapper/love_ga_wrapper/fitness_functions/meta_fitness_functions/SumFitnessFun.lua
+++ love_ga_wrapper/love_ga_wrapper/fitness_functions/meta_fitness_functions/SumFitnessFun.lua
@@ -4,11 +4,13 @@ local FitnessFun = require("love_ga_wrapper.fitness_functions.FitnessFun")
 -------------------------------------------------------------
 
 -- class: SumFitnessFun
+-- param: fitness_funs:list(FitnesFun) -> the list of fitness functions to calculate the sum from
 local SumFitnessFun = extend(FitnessFun, function(self, fitness_funs)
     self.fitness_funs = fitness_funs
 end)
 
 -- init: TASInput -> None
+-- initializes all fitness functions
 function SumFitnessFun.init(self, individual_tas)
     for _, fun in pairs(self.fitness_funs) do
         fun:init(individual_tas)
@@ -16,7 +18,7 @@ function SumFitnessFun.init(self, individual_tas)
 end
 
 -- mainFun: None -> num
--- The main fitness function, gets the difference between the current and the asteroid number
+-- The main fitness function, sums the results of all fitness functions calculated and returns it
 function SumFitnessFun.mainFun(self)
     local sum = 0
     
@@ -28,7 +30,7 @@ function SumFitnessFun.mainFun(self)
 end
 
 -- stepFun: None -> None
--- The step fitness function, does nothing
+-- The step fitness function, performs the step function on all fitness functions
 function SumFitnessFun.stepFun(self)
     for _, fun in pairs(self.fitness_funs) do
         fun:stepFun()
