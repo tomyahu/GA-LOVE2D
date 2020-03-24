5a79ac1bf2f46e55ba30cebf0b8c04d4d9fe460e
Tomás Ahumada <tomyahu@gmail.com>
2020-03-24 17:35:22 -0300

    moved sum fitness fun to meta fitness fucntion folder

---
+++ love_ga_wrapper/love_ga_wrapper/fitness_functions/meta_fitness_functions/SumFitnessFun.lua
@@ -0,0 +1,38 @@
+local class = require("love_ga_wrapper.lib.classes.class")
+local extend = require("love_ga_wrapper.lib.classes.extend")
+local FitnessFun = require("love_ga_wrapper.fitness_functions.FitnessFun")
+-------------------------------------------------------------
+
+-- class: SumFitnessFun
+local SumFitnessFun = extend(FitnessFun, function(self, fitness_funs)
+    self.fitness_funs = fitness_funs
+end)
+
+-- init: TASInput -> None
+function SumFitnessFun.init(self, individual_tas)
+    for _, fun in pairs(self.fitness_funs) do
+        fun:init(individual_tas)
+    end
+end
+
+-- mainFun: None -> num
+-- The main fitness function, gets the difference between the current and the asteroid number
+function SumFitnessFun.mainFun(self)
+    local sum = 0
+    
+    for _, fun in pairs(self.fitness_funs) do
+        sum = sum + fun:mainFun()
+    end
+    
+    return sum
+end
+
+-- stepFun: None -> None
+-- The step fitness function, does nothing
+function SumFitnessFun.stepFun(self)
+    for _, fun in pairs(self.fitness_funs) do
+        fun:stepFun()
+    end
+end
+
+return SumFitnessFun
