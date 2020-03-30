e3de31bbc7d0543dab94c32bf8e4ac2ec7bdf544
Tomás Ahumada <tomyahu@gmail.com>
2020-03-30 13:25:22 -0300

    added more variables to love tester

--- lib/genetic_algorithm/testers/LoveTester.py
+++ lib/genetic_algorithm/testers/LoveTester.py
@@ -1,6 +1,4 @@
-import sys
-
-from ga_settings.consts import love_path, frames_to_clean, frames_to_test, frames_to_skip
+from ga_settings.consts import love_path, frames_to_clean, frames_to_test, frames_to_skip, frames_interval
 from lib.genetic_algorithm.testers.Tester import Tester
 from lib.input_scripts.InputScript import InputScript
 from lib.os_lib.cd import cd
@@ -12,7 +10,9 @@ class LoveTester(Tester):
     Tester class for testing individuals and get their fitness. Runs the game and returns the resulting fitness.
     """
 
-    def __init__(self, aux_path):
+    def __init__(self, aux_path="out", clean_script="clean", skip_script="skip"):
+        self.skip_script = skip_script
+        self.clean_script = clean_script
         self.aux_path = aux_path
 
     def test(self, individual):
@@ -25,16 +25,18 @@ class LoveTester(Tester):
         # Get input script and save it as out
         original_input_script = individual.get_inputs()
         shifted_input_script = original_input_script.shift_frames(frames_to_skip)
-        skip_input_script = InputScript("individuals/skip")
+        skip_input_script = InputScript("individuals/" + self.skip_script)
 
         input_script = shifted_input_script + skip_input_script
         input_script.save_to_file("individuals/" + self.aux_path)
 
         with cd("./love_ga_wrapper"):
-            p = Popen([love_path, ".", "run_tas", "clean", str(frames_to_clean), str(sys.argv[1])], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            p = Popen([love_path, ".", "run_tas", self.clean_script, str(frames_to_clean), str(frames_interval)],
+                      stdin=PIPE, stdout=PIPE, stderr=PIPE)
             p.communicate()
 
-            p = Popen([love_path, ".", "run_tas", self.aux_path, str(frames_to_test + frames_to_skip), str(sys.argv[1])], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            p = Popen([love_path, ".", "run_tas", self.aux_path, str(frames_to_test + frames_to_skip),
+                       str(frames_interval)], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
             try:
                 out, err = p.communicate()
