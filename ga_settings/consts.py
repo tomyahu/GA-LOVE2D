ce8e03072c7e7cd41471bb76f7e65aa386f42198
Tomás Ahumada <tomyahu@gmail.com>
2020-04-03 13:14:43 -0300

    added hawkthorn testers

--- ga_settings/consts.py
+++ ga_settings/consts.py
@@ -1,7 +1,3 @@
-bla = '"C:\Program Files\love_versions\love-0.10.0-win64\love.exe" . run_game --level=village-forest --position=96,23'
-ble = '"C:\Program Files\love_versions\love-0.10.0-win64\love.exe" . run_game --level=village-forest-stonerspeak --position=125,33'
-bli = '"C:\Program Files\love_versions\love-0.10.0-win64\love.exe" . run_game --level=village-forest-stonerspeak --position=165,30'
-
 love_path = "C:\Program Files\love_versions\love-0.10.0-win64\love.exe"
 
 generations = 30
@@ -9,8 +5,8 @@ individual_num = 100
 mutation_prob = 0.05
 elitism_ratio = 0.05
 
-frames_to_clean = 200
-frames_to_skip = 3250
+frames_to_clean = 0
+frames_to_skip = 0
 frames_to_test = 500
 
-frames_interval = 0
+frames_interval = 4
