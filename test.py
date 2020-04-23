62ec05a3b5ff9ede465a7168413cbddb0f64419d
Tomás Ahumada <tomyahu@gmail.com>
2020-04-23 15:06:36 -0400

    deleted specific objects fot testing in Love 2D

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ from lib.os_lib.cd import cd
 from subprocess import Popen, PIPE, TimeoutExpired
 
 
-individual_path = "out_dir/gen_30"
+individual_path = "out_of_bounds"
 
 with cd("./love_ga_wrapper"):
 
-    p = Popen([love_path, ".", "run_tas", individual_path, str(frames_to_test), str(frames_to_skip), "1", "--level=" + "forest", "--position=" + str(116) + "," + str(11)], stdin=PIPE,
+    p = Popen([love_path, ".", "run_tas", individual_path, str(frames_to_test), str(frames_to_skip), "1"], stdin=PIPE,
               stdout=PIPE, stderr=PIPE)
 
     try:
