066fed1ba08cb949cda021858c06d61bf60c7979
Tomás Ahumada <tomyahu@gmail.com>
2020-06-19 17:33:21 -0400

    documented input script to individual lib class

--- lib/reduction/InputScriptToIndividualLib.py
+++ lib/reduction/InputScriptToIndividualLib.py
@@ -1,18 +1,34 @@
-from lib.genetic_algorithm.genomes.KeyFrameDurationGenome import KeyFrameDurationGenome
+from lib.genetic_algorithm.genes.KeyFrameDurationGene import KeyFrameDurationGene
 from lib.genetic_algorithm.individuals.InputKeyFrameDurationIndividual import InputKeyFrameDurationIndividual
 
 
 class InputScriptToIndividualLib:
     """
-    TODO: Document this class
+    Library class to transform input sequences to genetic algorithm individuals
     """
 
     @staticmethod
     def to_key_frame_duration_individual(input_script):
-        return InputKeyFrameDurationIndividual(InputScriptToIndividualLib.to_key_frame_duration_individual_aux(input_script))
+        """
+        Transform an input sequence into a KeyFrameDurationIndividual object
+        :param input_script: <InputScript> an input sequence object
+        :return: <InputKeyFrameDurationIndividual> a genetic algorithm individual that represents the input sequence
+                                                    given
+        """
+        return InputKeyFrameDurationIndividual(
+            InputScriptToIndividualLib.to_key_frame_duration_individual_aux(input_script)
+        )
 
     @staticmethod
     def to_key_frame_duration_individual_aux(input_script):
+        """
+        Auxiliary method for to_key_frame_duration_individual method
+        Transforms an input sequence into a list of key frame duration genes
+        :param input_script: <InputScript> an input sequence object
+        :return: <list(KeyFrameDurationGene)> a list of genes that represents the input sequence given
+        :param input_script:
+        :return:
+        """
         gene_list = list()
 
         input_dict = input_script
@@ -31,7 +47,7 @@ class InputScriptToIndividualLib:
                 else:
                     if input_key in aux_dict.keys():
                         gene_list.append(
-                            KeyFrameDurationGenome(input_key, aux_dict[input_key], frame - aux_dict[input_key]))
+                            KeyFrameDurationGene(input_key, aux_dict[input_key], frame - aux_dict[input_key]))
                         del aux_dict[input_key]
 
         return gene_list
